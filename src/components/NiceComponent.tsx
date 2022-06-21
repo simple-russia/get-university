719dee12682de0656134288ac2b42195cd1c04d0
simple-russia <77843094+simple-russia@users.noreply.github.com>
2022-06-22 00:07:58 +0300

    Pull request test (#1)
    * first commit to a new branch
    
    * 3rd commit of a branch
    
    * one more commit

---
+++ src/components/NiceComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface NiceComponentProps {}
+
+const NiceComponent = ({}: NiceComponentProps): JSX.Element => {
+  return (
+    <div>
+      This is some nice component
+      and lil bit more :)
+      another commit
+    </div>
+  );
+};
+
+export {NiceComponent};
