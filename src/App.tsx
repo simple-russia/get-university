0c216013479f01e60635612e9e75e81deb5ef6e4
Rombulus <alexeyisaev29032001@gmail.com>
2022-06-21 21:51:02 +0300

    first commit to a new branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import D from '../lang/en/global.json';
 import './styles/base.css';
 import './styles/variables.css';
+import {NiceComponent} from './components';
 
 const Tim = 4;
 console.log(Tim);
@@ -9,7 +10,10 @@ console.log(Tim);
 
 const App = () => {
   console.log(process.env.PORT);
-  return <p>{D.home.main_paragraph}</p>;
+  return <>
+    <p>{D.home.main_paragraph}</p>
+    <NiceComponent />
+  </>;
 };
 
 export {App};
---
+++ src/components/NiceComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface NiceComponentProps {}
+
+const NiceComponent = ({}: NiceComponentProps): JSX.Element => {
+  return (
+    <div>
+      This is some nice component
+    </div>
+  );
+};
+
+export {NiceComponent};
