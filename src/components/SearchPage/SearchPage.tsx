6d16d7dbaa114a74bab8bc6d79b7f3865e1c87b6
Rombulus <alexeyisaev29032001@gmail.com>
2022-06-22 19:53:37 +0300

    added search state

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Texts from 'lang/en/global.json';
 
 interface SearchPageProps {}
 
 const SearchPage = ({}: SearchPageProps): JSX.Element => {
+  const [searchString, setSearchString] = useState<string>('');
+
+  const onSearchStringInput = (event: React.FormEvent<EventTarget>): void => {
+    const target = event.target as HTMLInputElement;
+    setSearchString(target.value);
+  };
+
+  useEffect( () => {
+    console.log(searchString);
+  }, [searchString]);
+
   return (
     <div>
+      <input value={searchString} onChange={onSearchStringInput}></input>
       {Texts.home.main_paragraph}
     </div>
   );
