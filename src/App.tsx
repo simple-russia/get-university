2378338b1346990396131ed737e5be653ea426d9
Rombulus <alexeyisaev29032001@gmail.com>
2022-06-22 14:53:28 +0300

    a cleaner commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import D from '../lang/en/global.json';
+import Texts from '../lang/en/global.json';
 import './styles/base.css';
 import './styles/variables.css';
-import {NiceComponent} from './components';
 
 const Tim = 4;
 console.log(Tim);
@@ -11,8 +10,7 @@ console.log(Tim);
 const App = () => {
   console.log(process.env.PORT);
   return <>
-    <p>{D.home.main_paragraph}</p>
-    <NiceComponent />
+    <p>{Texts.home.main_paragraph}</p>
   </>;
 };
 
